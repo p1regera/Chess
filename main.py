fa55e0e3ad686a9d23adb9d44de003707c2e1499
Ango <ab9tm@virginia.edu>
2022-07-18 15:39:55 -0400

    Split functions into files
    Decided to split functions (not yet implemented) into separate files for convenience and project management.

--- main.py
+++ main.py
@@ -2,43 +2,7 @@ import pygame, sys
 from pygame.locals import *
 
 
-def CREATE_CHESSBOARD(window_width, window_height, surface):
-    rect_width = window_width / 8
-    rect_height = window_height / 8
-    x = y = 0
-    color = None
-    white = (255, 255, 255)
-    black = (0, 0, 0)
 
-    def create_horizontal_rects(x, y, color, color2):
-        for i in range(8):
-
-            rects = pygame.Rect(x, y, rect_width, rect_height)
-
-            if i % 2 == 0:
-                pygame.draw.rect(surface, color, rects)
-                x += rect_width
-
-            else:
-                pygame.draw.rect(surface, color2, rects)
-                x += rect_width
-
-    x = 0
-
-    for i in range(8):
-
-        if i % 2 == 0:
-            create_horizontal_rects(x, y, white, black)
-            y += rect_height
-
-        if i % 2 == 1:
-            create_horizontal_rects(x, y, black, white)
-            y += rect_width
-
-pygame.init()
-
-WIDTH = HEIGHT = 700
-WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
 
